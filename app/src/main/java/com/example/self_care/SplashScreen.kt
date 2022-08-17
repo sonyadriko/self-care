3205f314a6bd5c38e1b3a42c93cbcb2364e2c876
sonyadriko <sonyadiadriko@gmail.com>
2022-08-17 19:10:47 +0700

    activity login

--- app/src/main/java/com/example/self_care/SplashScreen.kt
+++ app/src/main/java/com/example/self_care/SplashScreen.kt
@@ -1,11 +1,20 @@
 package com.example.self_care
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        val buttonStarted = findViewById<Button>(R.id.buttonstarted);
+
+        buttonStarted.setOnClickListener{
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent);
+        }
     }
 }
