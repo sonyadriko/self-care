8ac02f26bb69e3c4761df784aa9205fe5b3eda9c
sonyadriko <sonyadiadriko@gmail.com>
2022-08-25 23:11:12 +0700

    result error

--- app/src/main/java/com/example/self_care/HomeFragment.kt
+++ app/src/main/java/com/example/self_care/HomeFragment.kt
@@ -1,10 +1,12 @@
 package com.example.self_care
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -34,8 +36,17 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+
+        val v = inflater.inflate(R.layout.fragment_home, container, false)
+        val btnStart = v.findViewById<Button>(R.id.buttonUjiAwal)
+
+        btnStart.setOnClickListener {
+            startActivity(Intent (activity, PreTestActivity::class.java))
+        }
+        return v
+
     }
+
 
     companion object {
         /**
