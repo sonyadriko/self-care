21c963613b5e9a925848c193d57a865b74ab52df
Ainul <ainul.y9b@gmail.com>
2022-08-30 14:38:13 +0700

    updates : - fix error ArtikelActivity.kt null object view - replace view synthetic with viewBinding

--- app/src/main/java/com/example/self_care/view/artikel/ArtikelActivity.kt
+++ app/src/main/java/com/example/self_care/view/artikel/ArtikelActivity.kt
@@ -1,52 +1,38 @@
 package com.example.self_care.view.artikel
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.ImageView
-import android.widget.TextView
 import android.widget.Toast
-import com.example.self_care.R
+import com.example.self_care.databinding.ActivityArtikelBinding
 import com.example.self_care.domain.Artikel
-import org.w3c.dom.Text
 import java.lang.Exception
 
 class ArtikelActivity : AppCompatActivity() {
-    private lateinit var artikelData : Artikel
+
+    private lateinit var binding: ActivityArtikelBinding
+    private var artikelData: Artikel? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityArtikelBinding.inflate(layoutInflater)
 
         initBundle()
         initView()
-
-        setContentView(R.layout.activity_artikel)
-
-
-    }
-
-    private fun initView() {
-        try {
-            val image = findViewById<ImageView>(R.id.image_artikel)
-            val heading = findViewById<TextView>(R.id.heading_artikel)
-            val content = findViewById<TextView>(R.id.news_artikel)
-
-            image.setImageResource(artikelData.titleImage)
-            heading.setText(artikelData.heading)
-            content.setText(artikelData.content)
-
-        }catch (e:Exception){
-            e.message?.let { Log.d("kosong", artikelData.titleImage.toString()) }
-            Toast.makeText(this, "terjadi kesalahan", Toast.LENGTH_SHORT).show()
-            finish()
-        }
+        setContentView(binding.root)
     }
 
     private fun initBundle() {
-        if (intent.hasExtra("heading")) {
-            val extra = intent.extras?.get("heading")
-            artikelData = extra as Artikel
-//            Toast.makeText(this, extra.toString(), Toast.LENGTH_SHORT).show()
+        if (intent.hasExtra("heading")) artikelData = intent.extras?.get("heading") as Artikel
+    }
+
+    private fun initView() = binding.apply {
+        try {
+            imageArtikel.setImageResource(artikelData!!.titleImage)
+            headingArtikel.setText(artikelData!!.heading)
+            newsArtikel.setText(artikelData!!.content)
+        } catch (e: Exception) {
+            Toast.makeText(this@ArtikelActivity, "terjadi kesalahan", Toast.LENGTH_SHORT).show()
+            finish()
         }
     }
 }
